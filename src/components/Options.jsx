49549dd1bf2802637019acd228911c655ff9925e
Ewusi David Abedu <150860194+bludojr@users.noreply.github.com>
2024-11-08 10:51:37 +0000

    made changes

--- src/components/Options.jsx
+++ src/components/Options.jsx
@@ -1,17 +1,17 @@
-import { NavLink, Outlet } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 const icons = [
-  { name: "HTML", bgClass: "bg-icon-html bg-icon-html-opacity", path: "html" },
-  { name: "CSS", bgClass: "bg-icon-css bg-icon-css-opacity", path: "css" },
-  { name: "JavaScript", bgClass: "bg-icon-js bg-icon-js-opacity", path: "js" },
-  { name: "Accessibility", bgClass: "bg-icon-accessibility bg-icon-accessibility-opacity", path: "accessibility" }
+  { name: "HTML", bgClass: "bg-icon-html bg-icon-html-opacity", path: "HTML" },
+  { name: "CSS", bgClass: "bg-icon-css bg-icon-css-opacity", path: "CSS" },
+  { name: "JavaScript", bgClass: "bg-icon-js bg-icon-js-opacity", path: "JavaScript" },
+  { name: "Accessibility", bgClass: "bg-icon-accessibility bg-icon-accessibility-opacity", path: "Accessibility" }
 ];
 
 const Objectives = ({ label, bgClass, path }) => {
   return (
     <NavLink
       to={path}
-      className="pl-6 flex space-x-5 shadow-md bg-white h-[75px] w-full rounded-3xl border border-white hover:border-fuchsia-500 hover:border-2"
+      className="pl-6 flex space-x-5 shadow-md bg-white h-[75px] w-full rounded-3xl border border-white "
     >
       <Picture bgClass={bgClass} />
       <div className="my-auto text-3xl font-medium text-dark-gray">{label}</div>
@@ -31,7 +31,6 @@ const Options = () => {
       {icons.map((icon, index) => (
         <Objectives key={index} label={icon.name} bgClass={icon.bgClass} path={icon.path} />
       ))}
-      <Outlet /> {/* This renders the nested routes within Options */}
     </div>
   );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -14,21 +14,25 @@ import CSS from "./pages/CSS";
 import JS from "./pages/JS"
 import Accessibility from "./pages/Accessibility";
 import RootLayout from "./layout/RootLayout";
+import { store } from "./app/store";
+import { Provider } from "react-redux";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <>
       <Route path="/" element={<RootLayout />} />
-      <Route path="html" element={<HTML />} />
-      <Route path="css" element={<CSS />} />
-      <Route path="js" element={<JS />} />
-      <Route path="accessibility" element={<Accessibility />} />
+      <Route path="HTML" element={<HTML />} />
+      <Route path="CSS" element={<CSS />} />
+      <Route path="JavaScript" element={<JS />} />
+      <Route path="Accessibility" element={<Accessibility />} />
     </>
   )
 );
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
+    <Provider store={store}>
     <RouterProvider router={router} />
+    </Provider>
   </StrictMode>
 );
